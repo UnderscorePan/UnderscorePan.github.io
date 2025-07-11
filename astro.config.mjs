bf642fcbdf108b90162ccb97f9bd916a05e64d91
NotAPan <hewweebo@gmail.com>
2025-07-11 17:49:29 +0800

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import astroIcon from 'astro-icon';
 
-// https://astro.build/config
-export default defineConfig({});
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  integrations: [astroIcon()],
+
+  site: 'https://UnderscorePan.github.io',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+})
